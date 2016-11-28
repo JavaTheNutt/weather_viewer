c9cdcda9855ffeb5e8456594192e9aeee94977b1
Joe Wemyss <JavaTheNutt@users.noreply.github.com>
2016-11-28 14:57:35 +0000

    added possible fixes to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path');
+//FIXME: uncomment and test webpack package
+//var webpack = require('webpack');
 //fixme: no output from operation
 module.exports = {
     entry: "./src/app/js/index.js",
     output: {
-        path: __dirname + 'dist/app',
+        path: __dirname + '/dist/app', //todo: check if preceding slash was causing the problem
         filename: "bundle.js"
     },
     module: {
@@ -19,7 +21,7 @@ module.exports = {
                 test: /\.ts(x?)$/,
                 loader: 'ts-loader',
                 include: [
-                    path.resolve(__dirname, 'src/app')
+                    path.resolve(__dirname, 'src/app/ts')
                 ]/!*,
                 exclude: /(node_modules)/!*!/
             }*/
@@ -39,6 +41,13 @@ module.exports = {
     resolve: {
         root:[
             path.resolve('./src/app')
-        ]
-    }
+        ]/*,
+        extensions:['', '.webpack.js', '.web.js', '.ts', '.js', '.scss']*/
+        //FIXME: uncomment and test extensions array
+    }/*,
+    devtool: 'source-map',
+    plugins:[
+        new webpack.optimize.UglifyJsPlugin()
+    ]*/
+    //FIXME: uncomment and test sourcemaps and minification
 };
